73d2bdac326b7eae1e67f59924a45781a2bcb218
Дмитрий Якорев <urakend@mail.ru>
2019-10-12 14:20:50 +0300

    success

--- src/index.js
+++ src/index.js
@@ -38,7 +38,80 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let w='';
+    let NEW_MORSE_TABLE = {' ':' '};
+
+
+    for (const i in MORSE_TABLE) {
+        
+        let v='';
+        //go through table
+        for (let index = 0; index < i.length; index++) {
+            const element = i[index];
+           if (element=='.') {v+='10'
+               
+           } else {v+='11'
+               
+           } 
+
+        }
+        NEW_MORSE_TABLE[v]=MORSE_TABLE[i];
+    
+       
+    };
+    for (let j = 0,k = 10; j < expr.length; j+=10,k+=10) {
+        let letter=expr.slice(j, k);
+     
+     
+    
+        for (let i= 0; i<10;i++) {
+          const element = letter[i];   
+          if (element === '*'){
+            w+=' ';
+            break;
+          };
+         if (element !== '0') {
+              let newletter=letter.slice(i,10);
+              // console.log('new letter',newletter);
+               
+
+
+              
+               
+              
+              
+              
+              
+            
+             for (let key in NEW_MORSE_TABLE) {
+             if(key === newletter){
+               w+=NEW_MORSE_TABLE[key];
+             
+               break;
+             } ;
+            
+            }
+            
+                break;
+        
+               } ;
+          
+                    
+      
+           }
+         
+           
+         
+       }
+    
+        
+ 
+     
+      
+            
+      
+  return w;
 }
 
 module.exports = {
